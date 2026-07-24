c20ed806659371b21c7a863e87e6705ffa7625d8
gbghazalabukhari1223 <ghazalabukhari01@outlook.com>
2026-07-24 23:38:14 +0500

    Start KB Jarvis OS with Version 14 work core

--- kb-jarvis-os/src/KB.Jarvis.App/App.xaml.cs
+++ kb-jarvis-os/src/KB.Jarvis.App/App.xaml.cs
@@ -29,7 +29,7 @@ public partial class App : Application
         base.OnStartup(e);
         var window = new MainWindow();
         MainWindow = window;
-        window.InitializeV13();
+        window.InitializeV14();
         window.Show();
     }
 }
